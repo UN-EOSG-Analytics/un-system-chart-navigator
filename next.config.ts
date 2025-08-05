032a1aba3d53f1d59840c7b16a1d2a1ee3c3d6d2
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-08-04 21:05:49 -0400

    try fix path resolve

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve('./src'),
+      '@/lib': path.resolve('./src/lib'),
+      '@/components': path.resolve('./src/components'),
+      '@/types': path.resolve('./src/types'),
+      '@/hooks': path.resolve('./src/hooks'),
     };
     return config;
   },
