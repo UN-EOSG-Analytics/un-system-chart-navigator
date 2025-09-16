83a447fcb56b3021fd7bd170fd5bec570efb76dd
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2025-09-16 10:41:38 -0400

    fix turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Avoid bundling d3-org-chart on the server
-      config.externals = config.externals || [];
-      config.externals.push('d3-org-chart', 'd3');
-    }
-    return config;
-  },
+  serverExternalPackages: ['d3-org-chart', 'd3']
 };
 
 export default nextConfig;
