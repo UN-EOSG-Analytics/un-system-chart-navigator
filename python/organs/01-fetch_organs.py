abdcf8e9054b097c90021bd008d7da41b044f5f8
kleinlennart <31488915+kleinlennart@users.noreply.github.com>
2026-02-19 10:52:56 -0500

    update organs

---
+++ python/organs/01-fetch_organs.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from api.airtable import fetch_airtable_table
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AIRTABLE_TABLE_ID = "tbl4mlFmIH4H7QVoX"
+
+df = fetch_airtable_table(AIRTABLE_TABLE_ID)
+
+df = df.dropna(how="all")
+df = df.sort_values("entity", ascending=True)
+
+df = df[df["is_displayed"] == "True"]
+
+
+df = df[
+    [
+        "entity",
+        "entity_long",
+        "governing_bodies",
+        "intergov_bodies_link",
+        "secretariats",
+        "order",
+        "system_grouping"
+    ]
+]
+
+output_path = Path("public/data/organ_contacts.json")
+output_path.parent.mkdir(parents=True, exist_ok=True)
+df.to_json(output_path, orient="records", indent=2)
+
+len(df)
